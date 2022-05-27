83edad3d07b035cea11f19cece25240f776a02cb
eshanHaque <eshan.haque16@gmail.com>
2022-05-28 03:11:22 +0600

    cors update

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
 const app = express();
 const port = process.env.port || 5000;
 
-app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+  app.options("*", cors(corsConfig))
+  app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+  next()
+  })
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxn8q.mongodb.net/?retryWrites=true&w=majority`;
