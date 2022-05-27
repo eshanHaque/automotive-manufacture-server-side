e0f43325d049a41b30fe2750f84101f8940fc4d8
eshanHaque <eshan.haque16@gmail.com>
2022-05-28 03:20:32 +0600

    heroku error fixing

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
-const corsConfig = {
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-  }
-  app.use(cors(corsConfig))
-  app.options("*", cors(corsConfig))
-  app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
-  next()
-  })
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxn8q.mongodb.net/?retryWrites=true&w=majority`;
