bd2da179f0c65f785be9bbefb579ed20355f7d32
eshanHaque <eshan.haque16@gmail.com>
2022-05-27 21:41:40 +0600

    admin role created

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ async function run() {
           return res.send({result});
         });
 
+
+        app.get('/user', async(req, res) => {
+          const users =await userCollection.find().toArray();
+          res.send(users);
+        })
+
+        
+
+        app.put('/user/admin/:email', async (req, res) => {
+          const email = req.params.email;
+          const filter = { email: email };
+          const updateDoc = {$set:{role: 'admin'},};
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+        })
+
+
         app.put('/user/:email', async (req, res) => {
           const email = req.params.email;
           const user = req.body;
